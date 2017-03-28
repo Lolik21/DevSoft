4170923734e5de8b82f103425a78836467f0ed8c
Lolik21 <cvobodne@gmail.com>
2017-03-29 00:15:22 +0300

    Чё я сделал?

---
+++ Email_Sender/Email_Sender/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Mail;
+using System.Net;
+
+namespace Email_Sender
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            MailAddress from = new MailAddress("cvobodne@yandex.ru");
+            MailAddress to = new MailAddress("cvobodn@yandex.ru");
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = "hello";
+            m.Body = "<h2>Привет как там дела?</h2>";
+            m.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("smtp.yandex.ru", 25);
+            smtp.Credentials = new NetworkCredential("cvobodne@yandex.ru", "JasdoFy123");
+            smtp.EnableSsl = true;
+            smtp.Timeout = 5;
+            smtp.Send(m); 
+        }
+    }
+}
